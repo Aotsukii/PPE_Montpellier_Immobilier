bd7c8e349fa4ac1faa32d8aa449d66dc38373655
Buck <elie@gazel.net>
2019-03-01 22:14:19 +0100

    01_03_19 index shape

--- index.php
+++ index.php
@@ -1,19 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Bienvenue chez Montpellier Immobilier</title>
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
-    <link rel="stylesheet" href="./rsc/mtp_stock.jpg">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">    <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap core CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.3/css/mdb.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
 <body>
-<section id="landingSection">
+   + <div +lass="section section0">
+  - +   <div class="container section0-inside">
+            <div class="row">
+                +6/8
+                <div class="login-box">
+                    <div class="input-section">
+                        <i class="far fa-user"></i>
+                        <input class="input-area" type="text" placeholder="Username">
+                    </div>
+                    <div class="input-section">
+                        <i class="fas fa-lock"></i>
+                        <input class="input-area" type="password" placeholder="Password">
+                    </div>
+                    <div class="input-section hidden">
+                        <i class="far fa-envelope"></i>
+                        <input class="input-area" type="text" placeholder="Email">
+                    </div>
+                    <button class="btn" id="login-btn">Login</button>
+                    <div class="question-box">
+                        <p class="question">Not a member?</p><a class="reg-btn" href="#">Register</a>
+                    </div>
+                </div>
+                <div class="success-box">
+                    <h1>You have been logged in!</h1>
+                </div>
+            </div>
 
-</section>
+        </div>
+    </div>
+
+
+
+<script type="text/javascript" src="./js/form.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!-- Bootstrap tooltips -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
